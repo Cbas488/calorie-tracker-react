98f296b148e3450f5259bba1368de4176d26667c
Darío Sebastián Zárate Ceballlos <zdario488@gmail.com>
2024-10-06 23:47:10 -0600

    Terminando proyecto

---
+++ src/reducers/activity.ts
@@ -0,0 +1,73 @@
+import type { Activity } from "../types"
+
+const localStorageActivities = (): Activity[] => {
+    const activities = localStorage.getItem('activities')
+
+    return activities ? JSON.parse(activities) : []
+}
+
+export type ActivityActions = {
+    type: 'save-activity',
+    payload: {
+        newActivity: Activity
+    }
+} | {
+    type: 'set-activeId',
+    payload: {
+        id: Activity['id']
+    }
+} | {
+    type: 'delete-activity',
+    payload: { id: Activity['id'] }
+} | { type: 'restart-app' }
+
+export type ActivityState = {
+    activities : Activity[],
+    activeId: Activity['id']
+}
+
+export const initialState: ActivityState = {
+    activities: localStorageActivities(),
+    activeId: ''
+}
+
+export const activityReducer = (state: ActivityState = initialState, actions: ActivityActions) => {
+    if(actions.type  === 'save-activity'){
+        let updatedActivities: Activity[] = []
+        
+        if(state.activeId) {
+            updatedActivities = state.activities.map(activity => activity.id === state.activeId ? actions.payload.newActivity : activity)
+        } else {
+            updatedActivities = [...state.activities, actions.payload.newActivity]
+        }
+
+        return {
+            ...state,
+            activities: updatedActivities,
+            activeId: ''
+        }
+    }
+
+    if(actions.type === 'set-activeId') {
+        return {
+            ...state,
+            activeId: actions.payload.id
+        }
+    } 
+
+    if(actions.type === 'delete-activity') {
+        return {
+            ...state,
+            activities: state.activities.filter(activity => activity.id !== actions.payload.id)
+        }
+    }
+
+    if(actions.type === 'restart-app') {
+        return {
+            activities: [],
+            activityId: ''
+        }
+    }
+
+    return state
+}
